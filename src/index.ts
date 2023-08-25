3aa92f55b1de63aee0ca4012bc1ca231eaf1c2b9
Duc Nguyen T. [4] VN.Danang <duc.nguyen4@monstar-lab.com>
2023-08-25 09:56:31 +0700

    test

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
 var variableA = [];
 var variableB = [1];
+var variableC = [1, 2];
+var variableD = [1, 2];
+var variableE = [
+  function foo() {
+    variableA = [];
+    variableB = [];
+    variableC = [];
+    variableD = [];
+  },
+];
