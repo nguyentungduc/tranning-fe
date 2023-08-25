b754566a601b24fabc3cf2cc1788194d1ccebedf
Duc Nguyen T. [4] VN.Danang <duc.nguyen4@monstar-lab.com>
2023-08-25 10:49:35 +0700

    Test code

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,18 @@
 module.exports = {
-  parserOptions: {
-    ecmaVersion: 2021,
-    sourceType: 'module',
-  },
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  parser: '@typescript-eslint/parser', // Sử dụng parser cho TypeScript
+  plugins: ['@typescript-eslint', 'react'], // Sử dụng các plugin cần thiết
+  extends: [
+    'eslint:recommended', // Sử dụng cấu hình cơ bản của ESLint
+    'plugin:@typescript-eslint/recommended', // Sử dụng cấu hình quy tắc khuyến nghị cho TypeScript
+    'plugin:react/recommended', // Sử dụng cấu hình quy tắc khuyến nghị cho React
+  ],
   rules: {
-    'no-unused-vars': [
-      'error',
-      {
-        vars: 'all',
-        args: 'none',
-        ignoreRestSiblings: false,
-        varsIgnorePattern: '^variable',
-      },
-    ],
+    // Các quy tắc riêng của bạn có thể thêm ở đây
+    // Ví dụ: 'no-unused-vars': 'error'
+  },
+  settings: {
+    react: {
+      version: '18', // Chỉ định phiên bản React bạn đang sử dụng
+    },
   },
 };
---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Title h1</h1>
+      <h2>Title h2</h2>
+      <h3>Title h3</h3>
+      <h4>Title h4</h4>
+      <h5>Title h5</h5>
+      <h6>Title h6</h6>
+      <ul className="row pt-10">
+        <li className="col-small-3">Column</li>
+        <li className="col-small-3">Column</li>
+        <li className="col-small-3">Column</li>
+        <li className="col-small-3">Column</li>
+        <li className="col-small-3">Column</li>
+        <li className="col-small-3">Column</li>
+        <li className="col-small-3">Column</li>
+        <li className="col-small-3">Column</li>
+      </ul>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Title 1</th>
+            <th>Title 2</th>
+            <th>Title 3</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Content 1</td>
+            <td>Content 2</td>
+            <td>Content 3</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
